38c63265387f8d35346177b8cad4dcedb3fce1b6
Yachim <jachym.kohout@gmail.com>
2022-10-16 21:22:03 +0200

    Remove constructor in User model

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -5,15 +5,8 @@ namespace DB.Models
     public class User
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-
-        public User(string username, string email, string password)
-        {
-            Username = username;
-            Email = email;
-            Password = password;
-        }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
     }
 }
