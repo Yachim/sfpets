b1b219d804b8258e02c6c297393fbb7b35af8d73
Yachim <jachym.kohout@gmail.com>
2023-02-12 07:14:15 +0100

    Format; change login menu from blur to black opacity overlay

--- client/src/components/pages/Page.tsx
+++ client/src/components/pages/Page.tsx
@@ -3,13 +3,13 @@ import { Params } from "../../types";
 import styles from "../../scss/Page.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faCircleUser,
-    faFire,
-    faLanguage,
-    faMoon,
-    faSeedling,
-    faSun,
-    faWater
+	faCircleUser,
+	faFire,
+	faLanguage,
+	faMoon,
+	faSeedling,
+	faSun,
+	faWater
 } from "@fortawesome/free-solid-svg-icons";
 import { desc, mainHeading, title } from "../../data/translation";
 import { CSSProperties, useEffect, useState } from "react";
@@ -17,22 +17,22 @@ import { LoginMenu } from "..";
 
 // TODO: implement hidden link
 export function Page(props: { children?: React.ReactNode }) {
-    const params = useParams<Params>();
-    const lang = params.lang!;
-    const element = params.element;
+	const params = useParams<Params>();
+	const lang = params.lang!;
+	const element = params.element;
 
-    const [searchParams] = useSearchParams();
+	const [searchParams] = useSearchParams();
 
-    document.title = title[lang];
-    document
-        .querySelector("meta[name='description']")
-        ?.setAttribute("content", desc[lang]);
+	document.title = title[lang];
+	document
+		.querySelector("meta[name='description']")
+		?.setAttribute("content", desc[lang]);
 
-    function generateUrl(
-        element: "shadow" | "light" | "earth" | "fire" | "water"
-    ) {
-        return `../${lang}/${element}?${searchParams.toString()}`;
-    }
+	function generateUrl(
+		element: "shadow" | "light" | "earth" | "fire" | "water"
+	) {
+		return `../${lang}/${element}?${searchParams.toString()}`;
+	}
 
 	const [loginShown, setLoginShown] = useState(false);
 	const [darkTheme, setDarkTheme] = useState(true);
@@ -47,17 +47,18 @@ export function Page(props: { children?: React.ReactNode }) {
 		document.documentElement.classList.add(themeClass);
 	}, [themeClass]);
 
-    return (
-        <div
+	return (
+		<div
 			className={styles.app}
 			data-login-open={loginShown}
 		>
-			<LoginMenu shown={loginShown} closeFunc={setLoginShown}/>
+			<LoginMenu shown={loginShown} closeFunc={setLoginShown} />
+			<div className={styles["modal-overlay"]} data-visible={loginShown} />
 
-            <header className={styles["top-bar"]}>
-                <Link to="../">
-                    <h1>{mainHeading[lang]}</h1>
-                </Link>
+			<header className={styles["top-bar"]}>
+				<Link to="../">
+					<h1>{mainHeading[lang]}</h1>
+				</Link>
 				<div className={styles["user-settings"]}>
 					<button
 						className={styles["user-settings-button"]}
@@ -77,51 +78,51 @@ export function Page(props: { children?: React.ReactNode }) {
 						<FontAwesomeIcon icon={faCircleUser} />
 					</button>
 				</div>
-            </header>
+			</header>
 
-            <nav className={styles["nav-bar"]}>
-                <Link
-                    to={generateUrl("shadow")}
-                    data-active={element === "shadow"}
-                    className={styles.icon}
-					style={{"--hover-color": "gray"} as CSSProperties}
-                >
-                    <FontAwesomeIcon icon={faMoon} />
-                </Link>
-                <Link
-                    to={generateUrl("light")}
-                    data-active={element === "light"}
-                    className={styles.icon}
-					style={{"--hover-color": "yellow"} as CSSProperties}
-                >
-                    <FontAwesomeIcon icon={faSun} />
-                </Link>
-                <Link
-                    to={generateUrl("earth")}
-                    data-active={element === "earth"}
-                    className={styles.icon}
-					style={{"--hover-color": "green"} as CSSProperties}
-                >
-                    <FontAwesomeIcon icon={faSeedling} />
-                </Link>
-                <Link
-                    to={generateUrl("fire")}
-                    data-active={element === "fire"}
-                    className={styles.icon}
-					style={{"--hover-color": "orangered"} as CSSProperties}
-                >
-                    <FontAwesomeIcon icon={faFire} />
-                </Link>
-                <Link
-                    to={generateUrl("water")}
-                    data-active={element === "water"}
-                    className={styles.icon}
-					style={{"--hover-color": "lightskyblue"} as CSSProperties}
-                >
-                    <FontAwesomeIcon icon={faWater} />
-                </Link>
-            </nav>
-            {props.children}
-        </div>
-    );
+			<nav className={styles["nav-bar"]}>
+				<Link
+					to={generateUrl("shadow")}
+					data-active={element === "shadow"}
+					className={styles.icon}
+					style={{ "--hover-color": "gray" } as CSSProperties}
+				>
+					<FontAwesomeIcon icon={faMoon} />
+				</Link>
+				<Link
+					to={generateUrl("light")}
+					data-active={element === "light"}
+					className={styles.icon}
+					style={{ "--hover-color": "yellow" } as CSSProperties}
+				>
+					<FontAwesomeIcon icon={faSun} />
+				</Link>
+				<Link
+					to={generateUrl("earth")}
+					data-active={element === "earth"}
+					className={styles.icon}
+					style={{ "--hover-color": "green" } as CSSProperties}
+				>
+					<FontAwesomeIcon icon={faSeedling} />
+				</Link>
+				<Link
+					to={generateUrl("fire")}
+					data-active={element === "fire"}
+					className={styles.icon}
+					style={{ "--hover-color": "orangered" } as CSSProperties}
+				>
+					<FontAwesomeIcon icon={faFire} />
+				</Link>
+				<Link
+					to={generateUrl("water")}
+					data-active={element === "water"}
+					className={styles.icon}
+					style={{ "--hover-color": "lightskyblue" } as CSSProperties}
+				>
+					<FontAwesomeIcon icon={faWater} />
+				</Link>
+			</nav>
+			{props.children}
+		</div>
+	);
 }
